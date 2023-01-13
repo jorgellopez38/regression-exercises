43cb08285115674fa59cd952ede87f01a52a17a3
Jorge <jorge.l.lopez38@gmail.com>
2023-01-12 22:40:48 -0600

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -78,3 +78,36 @@ def wrangle_zillow():
     train, validate, test = prep_zillow(acquire_zillow())
     
     return train, validate, test
+
+def scale_data(train, 
+               validate, 
+               test, 
+               scale_cols=['bedrooms', 'bathrooms', 'tax_amount', 'sq_feet'],
+               return_scaler=False):
+    '''
+    Scales the 3 data splits. 
+    Takes in train, validate, and test data splits and returns them scaled.
+    If return_scalar is True, the scaler object will be returned as well
+    '''
+    # make copies of the original data so no leakage
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    # variable 
+    mm_scaler = MinMaxScaler()
+    # fit it to scaler
+    mm_scaler.fit(train[columns_to_scale])
+    # scaling train, validate, test, and columns
+    train_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
+                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+                                                  
+    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
+    
+    if return_scaler:
+        return mm_scaler, train_scaled, validate_scaled, test_scaled
+    else:
+        return train_scaled, validate_scaled, test_scaled
