6ec606607b41f29f439826bb3a0e15f139c38269
Jorge <jorge.l.lopez38@gmail.com>
2023-01-17 22:37:34 -0600

    wrangle

--- wrangle.py
+++ wrangle.py
@@ -6,19 +6,6 @@ from sklearn.model_selection import train_test_split
 from env import username, password, host 
 
 
-#def delete_outliers(df):
-#    '''This function deletes the outliers that are unrealistic for most of zillow customers'''
-#    df = df[df.bathrooms <= 6]
-    
-#    df = df[df.bedrooms <= 6]
-
-#    df = df[df.tax_value < 2_000_000]
-
-#    df = df[df.square_feet < 10000]
-
-#    return df
-
-
 def acquire_zillow():
     '''
     This function checks to see if zillow.csv already exists, 
@@ -62,6 +49,13 @@ def prep_zillow(df):
     df["bedrooms"] = df["bedrooms"].astype(int)    
     df["tax_value"] = df["tax_value"].astype(int)
     df["square_feet"] = df["square_feet"].astype(int)
+    
+    #delete outliers  
+    df = df[df.bathrooms <= 6]
+    df = df[df.bedrooms <= 6]
+    df = df[df.tax_value < 2_000_000]
+    df = df[df.square_feet < 10000]
+
     
     # train/validate/test split
     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
